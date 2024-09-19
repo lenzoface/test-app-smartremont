96d53f5979045fae5d1ee200693222b024a5893b
Sherkhan K <sherkhan4work@gmail.com>
2024-09-20 01:04:15 +0500

    added a light promary color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,17 +1,17 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
-// Create a theme with red as the primary color
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#00004B',
+      main: "#00004B", // темно синий
+      light: "#eaedf4", // серый
     },
     secondary: {
-      main: '#D32F2F',
+      main: "#D32F2F", // красный
     },
   },
   typography: {
-    fontFamily: 'Roboto',
+    fontFamily: "Roboto",
   },
   shape: {
     borderRadius: 0, // Set borderRadius to 0 globally
@@ -21,7 +21,7 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           // Any global styles you want for buttons can be added here
-          textTransform: 'none',  // Example: Disable uppercase transformation
+          textTransform: "none", // Example: Disable uppercase transformation
         },
       },
     },
